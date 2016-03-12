616b457d934f30febd1a1fda49c28dc696ec2ba7
Madhav <madhav.khanna@tothenew.com>
2016-03-12 23:45:48 +0530

    L12 Completed

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,11 +1,14 @@
 import {Component} from 'angular2/core';
+import {MyComponentComponent} from "./my-component.component";
 
 @Component({
     selector: 'my-app',
     template: `
         <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
+        <p>Hello World! gf</p>
+        <my-app></my-app>
     `,
+    directives : [MyComponentComponent]
 })
 export class AppComponent {
 
---
+++ dev/my-component.component.ts
@@ -0,0 +1,14 @@
+import {Component} from "angular2/core";
+
+@Component({
+
+    selector : "my-app",
+    template : `
+        <h1>My 1st Component</h1>
+        <p>First Component Example</p>
+    `,
+})
+
+export class MyComponentComponent {
+
+}
