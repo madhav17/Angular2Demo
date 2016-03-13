9642c67eaac0ca1791486c90e26f901ebfa84c63
Madhav <madhav.khanna@tothenew.com>
2016-03-13 09:23:58 +0530

    Code Refactor

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from 'angular2/core';
 import {MyComponentComponent} from "./my-component.component";
 
 @Component({
-    selector: 'my-app',
+    selector: 'app',
     template: `
         <h1>Angular 2 Boilerplate</h1>
         <p>Hello World! gf</p>
