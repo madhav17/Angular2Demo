03aef7db3c6c023c7bf557bafd172f2cb7dc59ec
Madhav <madhav.khanna@tothenew.com>
2016-03-13 10:01:37 +0530

    L13 completed

--- dev/my-component.component.ts
+++ dev/my-component.component.ts
@@ -5,10 +5,19 @@ import {Component} from "angular2/core";
     selector : "my-app",
     template : `
         <h1>My 1st Component</h1>
-        <p>First Component Example vb</p>
+        <p>Hi. I'm {{name}} and this is my 1st Angular Component.
+            <span [class.is-awesome]="inputElement.value === 'yes'">It's so awesome!</span>
+
+            <br/>
+            <br/>
+            Is it Awesome ?
+            <input type="text" #inputElement (keyup)="0">
+        </p>
     `,
+    //styles:[],
+    styleUrls:['src/css/mycomponent.css']
 })
 
 export class MyComponentComponent {
-
+    name = "Madhav"
 }
