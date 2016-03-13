62f806a6462002524f04cba3e31a03be1058e418
Madhav <madhav.khanna@tothenew.com>
2016-03-14 00:19:06 +0530

    Refactor

--- dev/my-component.component.ts
+++ dev/my-component.component.ts
@@ -2,20 +2,23 @@ import {Component} from "angular2/core";
 
 @Component({
 
-    selector : "my-app",
-    template : `
+    selector: "my-app",
+    template: `
         <h1>My 1st Component</h1>
-        <p>Hi. I'm {{name}} and this is my 1st Angular Component.
+        <p>Hi. I'm <span [style.color]="inputElement.value==='yes' ? 'blue' : '' ">{{name}}</span> and this is my 1st Angular Component.
             <span [class.is-awesome]="inputElement.value === 'yes'">It's so awesome!</span>
 
             <br/>
             <br/>
             Is it Awesome ?
             <input type="text" #inputElement (keyup)="0"/>
+            <br/>
+            <br/>
+            <button [disabled]="inputElement.value!=='yes'">Only enabled if 'yes' was entered</button>
         </p>
     `,
     //styles:[],
-    styleUrls:['src/css/mycomponent.css']
+    styleUrls: ['src/css/mycomponent.css']
 })
 
 export class MyComponentComponent {
