d03b95e98cc7994ad15316b65c0d4cde68becb65
Madhav <madhav.khanna@tothenew.com>
2016-03-14 00:20:48 +0530

    Refactor

--- dev/my-component.component.ts
+++ dev/my-component.component.ts
@@ -5,7 +5,7 @@ import {Component} from "angular2/core";
     selector: "my-app",
     template: `
         <h1>My 1st Component</h1>
-        <p>Hi. I'm <span [style.color]="inputElement.value==='yes' ? 'blue' : '' ">{{name}}</span> and this is my 1st Angular Component.
+        <p>Hi. I'm <span [style.color]="inputElement.value==='yes' ? 'blue' : '' ">{{name}}</span> and my 1st Angular Component.
             <span [class.is-awesome]="inputElement.value === 'yes'">It's so awesome!</span>
 
             <br/>
