08d5c88ef6a1c8f0a42cd2543da816b01592a6b9
Madhav <madhav.khanna@tothenew.com>
2016-03-18 23:02:39 +0530

    L21 completed

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,17 +1,30 @@
 import {Component} from "angular2/core";
-import {MyPuzzle} from "./puzzle/puzzle.component";
 
 @Component({
 
-    selector : "app",
-    template : `
-        <h1> Root Component </h1>
-        <my-puzzle></my-puzzle>
+    selector: "app",
+    template: `
+        {{'Hey'}}
+        <br/>
+        {{1==1}}
+        <br/>
+        {{name}}
+        <br/>
+        {{onTest}}
+        <br/>
+        {{onTest()}}
+        <br/>
+        <input type="text" value="{{name}}" />
+        <br/>
+        <input type="text" value="{{name}}" class="{{'red'}}"/>
     `,
-    directives:[MyPuzzle]
 
 })
 
-export class AppComponent{
+export class AppComponent {
+    name = "Maddy"
 
+    onTest():boolean {
+        return 1 == 1;
+    }
 }
