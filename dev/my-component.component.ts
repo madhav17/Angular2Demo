cdd5cd026be3e9fba83f8d38573a5be975f96edb
Madhav <madhav.khanna@tothenew.com>
2016-03-18 13:03:10 +0530

    L15 completed

--- dev/my-component.component.ts
+++ dev/my-component.component.ts
@@ -1,4 +1,6 @@
 import {Component} from "angular2/core";
+import {TestComponent} from "./test.component";
+import {OnInit} from "angular2/core";
 
 @Component({
 
@@ -16,11 +18,26 @@ import {Component} from "angular2/core";
             <br/>
             <button [disabled]="inputElement.value!=='yes'">Only enabled if 'yes' was entered</button>
         </p>
+        <test></test>
     `,
     //styles:[],
-    styleUrls: ['src/css/mycomponent.css']
+    styleUrls: ['src/css/mycomponent.css'],
+    directives : [TestComponent]
 })
 
-export class MyComponentComponent {
-    name = "Madhav"
+export class MyComponentComponent implements  OnInit{
+    //name = "Madhav"
+    // we want to get name dynamically in our real app
+    name : string;
+    //constructor(){
+    //    this.name = "Maddy"
+    //}
+    // but we do not want to use constructor
+
+// we use lifecycle hooks for it
+
+    // call when component is initialized
+    ngOnInit():any {
+        this.name = "Maddy"
+    }
 }
---
+++ dev/test.component.ts
@@ -0,0 +1,14 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+
+    selector : 'test',
+    templateUrl : 'templates/ts.tpl.html'
+//    keep space while giving the name of external files
+})
+
+
+export class TestComponent {
+
+}
