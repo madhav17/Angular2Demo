a239fc43a8f0708f9c1f0ac16b60cb8fbfc5175c
Madhav <madhav.khanna@tothenew.com>
2016-03-20 17:51:13 +0530

    All Examples is added

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,31 +1,11 @@
-import {Component} from "angular2/core";
-import {PropertyBindComponent} from "./property-bind.component";
+import {Component} from 'angular2/core';
+import {DataBindingComponent} from "./databinding.component";
 
 @Component({
-
-    selector: "app",
-    template: `
-    <section class="parent">
-        <h1>Parent Component</h1>
-        <h4>Please Enter your Name</h4>
-        <input type="text" [(ngModel)]="name">
-
-    <br/><br/><br/>
-    <!--<p>{{name}}</p>-->
-    <section class="child">
-        <!--<my-prop-bind [myName]="name"></my-prop-bind>-->
-        <my-prop-bind [myName]="name" [myAge]="25" (hobbiesChanged)= "hobbies = $event" ></my-prop-bind>
-    </section>
-    <p>My Hobbies are : {{hobbies}}  </p>
-    </section>
-
-
-    `,
-    directives: [PropertyBindComponent]
+    selector: 'app',
+    template: '<data-binding></data-binding>',
+    directives: [DataBindingComponent]
 })
-
 export class AppComponent {
 
-    name:string = '';
-    hobbies:string = '';
 }
