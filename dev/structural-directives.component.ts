e550f5b9749b55c1d3f58c720cbc058c8c939e57
Madhav <madhav.khanna@tothenew.com>
2016-03-25 19:37:03 +0530

    L36

--- dev/structural-directives.component.ts
+++ dev/structural-directives.component.ts
@@ -2,9 +2,32 @@ import {Component} from "angular2/core"
 
 
 @Component({
-    selector : 'structural-directive',
-    template : `
-        s
+    selector: 'structural-directive',
+    template: `
+        <section class="directive">
+
+            <h2>*ngIf</h2>
+
+            <div>
+                Enter a number higher than 10
+                <br/>
+                <input type="text" #number (keyup)="0"/>
+            </div>
+            <br/>
+            <!--we can also call fuction in double code of ngIf or any expression that result in true or false-->
+
+            <div *ngIf="number.value > 10">
+                <h5>Ho Gaya</h5>
+                Number is greater than 10
+            </div>
+
+             <div *ngIf="number.value < 10 && number.value != '' ">
+                <h5>Ho Gaya</h5>
+                Number is less than 10
+            </div>
+
+
+        </section>
     `,
 })
 
