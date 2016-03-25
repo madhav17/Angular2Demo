c1f9377514d934d22134b1d134e158d771e4ac3f
Madhav <madhav.khanna@tothenew.com>
2016-03-25 19:27:39 +0530

    L35 complete

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from "angular2/core";
 import {AttributeDirectives} from "./attribute-driectives.component";
+import {StructuralDirective} from "./structural-directives.component";
 
 @Component({
 
@@ -7,8 +8,11 @@ import {AttributeDirectives} from "./attribute-driectives.component";
     template : `
         <h1> Root Component </h1>
         <attr-directive></attr-directive>
+        <br/><br/>
+        <h1>Structural Component</h1>
+        <structural-directive></structural-directive>
     `,
-    directives : [AttributeDirectives]
+    directives : [AttributeDirectives,StructuralDirective]
 
 })
 
---
+++ dev/structural-directives.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "angular2/core"
+
+
+@Component({
+    selector : 'structural-directive',
+    template : `
+        s
+    `,
+})
+
+export class StructuralDirective {
+
+}
