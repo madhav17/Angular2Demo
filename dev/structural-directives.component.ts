7aa178fdd0ede21768ea4446625e99f60cf30d64
Madhav <madhav.khanna@tothenew.com>
2016-03-25 22:07:16 +0530

    L38 completed

--- dev/structural-directives.component.ts
+++ dev/structural-directives.component.ts
@@ -4,25 +4,44 @@ import {Component} from "angular2/core"
 @Component({
     selector: 'structural-directive',
     template: `
-        <section class="directive">
+        <section>
 
-            <h2>*ngFor</h2>
+        <h2>[ngSwitch]</h2>
+        <div>
+            Enter red,blue or green
+            <br>
+            <input type="text" #color (keyup)="0">
+        </div>
 
-            <div>
-                <ul>
-                <!--It repeats it till the condition is satisfied-->
-                <!--index is anothor variable this ngFor provide us and it is bind to local vairable counter-->
-                    <li *ngFor="#item of list, #counter = index">
-                        {{item}}  {{counter}}
-                    </li>
-                </ul>
-            </div>
+        <!--we use square brackets with ngSwitch becoz we are not specifying what should happen on different state -->
+        <!--we should specify that state individual by specifing template-->
+        <!--template is html 5 tag and each template will get ngSwitchWhen directive-->
 
+        <!--to handle default else default will always be printed-->
+        <div *ngIf="color.value != ''">
+        <div [ngSwitch]="color.value">
+            <template [ngSwitchWhen]="'red'">
+                Color is red
+            </template>
+
+            <template [ngSwitchWhen]="'blue'">
+                Color is blue
+            </template>
+
+            <template [ngSwitchWhen]="'green'">
+                Color is green
+            </template>
+
+            <template ngSwitchDefault>
+                Default
+            </template>
+        </div>
+        </div>
         </section>
     `,
 })
 
 export class StructuralDirective {
 
-    public list : Array<string> = ['Apple','Milk','Bread'];
+    public list:Array<string> = ['Apple', 'Milk', 'Bread'];
 }
